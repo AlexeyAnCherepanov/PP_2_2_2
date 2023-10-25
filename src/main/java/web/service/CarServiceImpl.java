86d87f75d3609e62d69125c80f46f14be0d195e7
awfullThingz <116511523+awfullThingz@users.noreply.github.com>
2023-10-25 20:25:30 +0300

    1

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,37 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+    private List<Car> cars;
+
+    public CarServiceImpl() {
+        cars = new ArrayList<>();
+        cars.add(new Car("BMW", 1234, "Black"));
+        cars.add(new Car("Mercedes", 5678, "Silver"));
+        cars.add(new Car("Mitsubishi", 912, "White"));
+        cars.add(new Car( "KIA", 20, "Red"));
+        cars.add(new Car( "UAZ", 94165161, "Green"));
+    }
+
+
+    @Override
+    public List<Car> getAllCars() {
+        return cars;
+    }
+
+    @Override
+    public List<Car> getCars(int count) {
+        if (count >= cars.size() || count <= 0) {
+            return cars;
+        } else {
+            return cars.subList(0, count);
+        }
+    }
+}
